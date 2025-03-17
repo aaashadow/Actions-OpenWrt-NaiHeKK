5369135ee2ae21c2ace5d59bb121a54f5c7e7b59
YuanKK <2197960230@qq.com>
2025-03-18 01:29:46 +0800

    自用

--- diy-part2.sh
+++ diy-part2.sh
@@ -79,9 +79,9 @@ AddPackage() {
     dir_name=$(basename "$dir")
     echo "dir_name: ${dir_name}"
     echo "FEEDS_LUCI: $FEEDS_LUCI"
-    find "$FEEDS_LUCI" -type d
+    find "$FEEDS_LUCI" -type d -name "luci"
     echo "1...."
-    find "${FEEDS_LUCI}" -type d -name "${dir_name}" -print0
+    find "package/feeds" -type d -name "luci" -print0
     echo "2...."
     find "${FEEDS_LUCI}" -type d -name "${dir_name}" -print0
     find "${FEEDS_LUCI}" -type d -name "${dir_name}" -print0 | xargs -0 rm -rf
