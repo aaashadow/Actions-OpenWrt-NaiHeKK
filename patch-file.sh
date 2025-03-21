30eb50f7de5aeaa6ab8340a39d384010ae7dcfa6
YuanKK <2197960230@qq.com>
2025-03-21 21:16:38 +0800

    自用

--- patch-file.sh
+++ patch-file.sh
@@ -8,10 +8,8 @@ function Patch_File() {
   patch_file_path=$2
   base_path=$PWD
   cp -r "../Patch-Files/$patch_file_path" "$parent_path"
-  ls -R "$parent_path"
   cd "$parent_path"
   for packagepatch in "$patch_file_path"/*; do
-    # 检查是否是普通文件 (可选，但推荐)
     if [ -f "$packagepatch" ]; then
       patch_file=$(basename "$packagepatch")
       ECHO Applying Patch File "$patch_file"
@@ -19,6 +17,7 @@ function Patch_File() {
     fi
   done
   cd "$base_path"
+  ECHO $PWD
 }
 
 cat feeds/packages/lang/rust/Makefile
